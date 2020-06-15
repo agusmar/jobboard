818afe33451bd5b9a12f5f36acfe0f1bb873cb72
Agus Mar <agustinam@gmail.com>
2020-06-15 12:10:20 -0300

    created functions in main.js

--- design/main.js
+++ design/main.js
@@ -1,7 +1,45 @@
+const jobOffer = {
+  "id": 1,
+  "company": "Photosnap",
+  "logo": "./images/photosnap.svg",
+  "new": true,
+  "position": "Senior Frontend Developer",
+  "role": "Frontend",
+  "postedAt": "1d ago",
+  "contract": "Full Time",
+  "location": "USA Only",
+  "languages": ["HTML", "CSS", "JavaScript"],
+  "tools": []
+};
+
+const cardsContainer = document.getElementById("cards-container");
+function jobTag(company, isNew) {
+  return `
+    <div class="job_tag">
+    <ul>
+      <li class='company'>${company}</li>
+      ${isNew ? "<li class='new-old'>NEW!</li>" : "" } 
+    </ul>
+  </div>
+  `
+};
+
+function cardTemplate(jobOffer) {
+  return `
+  <article class="card">
+    <div class="card-job">
+    ${jobTag(jobOffer.company, jobOffer.new)}
+    </div>
+  </article>
+  `
+};
+
+cardsContainer.innerHTML = cardTemplate(jobOffer);
+
 fetch("./data.json")
-  .then(function(resp) {
-  return resp.json();
+  .then(function (resp) {
+    return resp.json();
   })
-  .then(function(data) {
-    console.log(data)
+  .then(function (data) {
+    console.log(data);
   });
